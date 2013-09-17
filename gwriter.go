d03e6cf96f077fa6943441f2232887d48cf41868
Aditya Mukerjee <dev@chimeracoder.net>
2013-09-17 19:57:18 -0400

    Implement initialization of the 10 wheels

--- gwriter.go
+++ gwriter.go
@@ -1,109 +1,118 @@
 package main
 
 import (
-    "log"
-    "errors"
+	"errors"
+	"log"
 )
 
 var wheels []*Wheel
 
+var wheel_values = [][]int{
+	[]int{1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0},
+	[]int{1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0},
+	[]int{0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1},
+	[]int{0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0},
+	[]int{1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+	[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+	[]int{1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0},
+	[]int{1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+	[]int{1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1},
+	[]int{0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1},
+}
 
 var alphabet = map[string]int{
-    "2" : 0,
-    "T" : 1,
-    "3" : 2,
-    "O" : 3, 
-    "4" : 4,
-    "H" : 5,
-    "N" : 6,
-    "M" : 7,
-    "5" : 8,
-    "L" : 9,
-    "R" : 10,
-    "G" : 11,
-    "I" : 12,
-    "P" : 13,
-    "C" : 14,
-    "V" : 15,
-    "E" : 16,
-    "Z" : 17,
-    "D" : 18,
-    "B" : 19,
-    "S" : 20,
-    "Y" : 21,
-    "F" : 22,
-    "X" : 23,
-    "A" : 24,
-    "W" : 25,
-    "J" : 26,
-    "6" : 27,
-    "U" : 28,
-    "Q" : 29,
-    "K" : 30,
-    "7" : 31,
+	"2": 0,
+	"T": 1,
+	"3": 2,
+	"O": 3,
+	"4": 4,
+	"H": 5,
+	"N": 6,
+	"M": 7,
+	"5": 8,
+	"L": 9,
+	"R": 10,
+	"G": 11,
+	"I": 12,
+	"P": 13,
+	"C": 14,
+	"V": 15,
+	"E": 16,
+	"Z": 17,
+	"D": 18,
+	"B": 19,
+	"S": 20,
+	"Y": 21,
+	"F": 22,
+	"X": 23,
+	"A": 24,
+	"W": 25,
+	"J": 26,
+	"6": 27,
+	"U": 28,
+	"Q": 29,
+	"K": 30,
+	"7": 31,
 }
 
-
-
-type Wheel struct{
-    Items []int
-    CurrentIndex int  //Starts at zero
-    MaxSize int
+type Wheel struct {
+	Items        []int
+	CurrentIndex int //Starts at zero
+	MaxSize      int
 }
 
+func NewWheel(items []int) (w *Wheel) {
+	w = new(Wheel)
+	w.MaxSize = len(items)
 
-func NewWheel(max_size int) (w *Wheel){
-    w = new(Wheel)
-    w.MaxSize = max_size
+	w.Items = items
 
-    w.Items = []int{1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0}
-
-    w.CurrentIndex = 0
-    return w
+	w.CurrentIndex = 0
+	return w
 
 }
 
-func (w *Wheel) CurrentBit() (bit int){
-    bit = w.Items[w.CurrentIndex]
-    w.CurrentIndex = (w.CurrentIndex + 1) % w.MaxSize
-    return 
+func (w *Wheel) CurrentBit() (bit int) {
+	bit = w.Items[w.CurrentIndex]
+	w.CurrentIndex = (w.CurrentIndex + 1) % w.MaxSize
+	return
 }
-
-
-
 
 //EncryptCharacter takes a single character and encrypts it with all ten wheels in Wheels
 func EncryptCharacter(char string) error {
 
-    c, ok := alphabet[char]
-    if !ok {
-        return errors.New("error: character not in alphabet")
-    }
-    return nil
+	c, ok := alphabet[char]
+	if !ok {
+		return errors.New("error: character not in alphabet")
+	}
+	return nil
 
-    var i uint8
-    for i = 0; i < 5; i++ {
-        c = (c ^ wheels[i].CurrentBit()) << (4-i) //
-    }
-    
-    return nil
+	var i uint8
+	for i = 0; i < 5; i++ {
+		c = (c ^ wheels[i].CurrentBit()) << (4 - i) //
+	}
+
+	return nil
 }
 
+func main() {
+	//TODO initialize Wheels
 
+	wheels = make([]*Wheel, 10)
 
-func main(){
-    //TODO initialize Wheels
-
-    wheels = make([]*Wheel, 10)
-
-    for i := 0; i <10; i++ {
-        wheels[i] = NewWheel(47)
+    for i := 0; i < 10; i++{
+        wheels[i] = NewWheel(wheel_values[i])
     }
 
 
-    for j := 0; j < 10; j++ {
-        log.Print(wheels[0].CurrentBit())
+	for j := 0; j < 10; j++ {
+		log.Print(wheels[0].CurrentBit())
+	}
+
+    for _, wheel := range wheels {
+        log.Print(wheel.Items)
+        
     }
 
-    log.Print(1 ^ 1)
+	log.Print(1 ^ 1)
 }
