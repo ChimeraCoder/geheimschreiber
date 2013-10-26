2bc0cdc047ef1aac08239f6d172814088236929e
aditya <dev@chimeracoder.net>
2013-10-26 19:20:17 -0400

    Add test file to test for correct decryption (cracking) of message

---
+++ geheimschreiber_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "log"
+    "testing"
+)
+
+const TEST_CIPHERTEXT_FILE = "test_ciphertext.txt"
+
+var TEST_CIPHERTEXT_SOLVED_WHEELS = []*Wheel{NewWheel([]int{0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1}),
+	NewWheel([]int{0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0}),
+	NewWheel([]int{1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0}),
+	NewWheel([]int{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1}),
+	NewWheel([]int{1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0}),
+	NewWheel([]int{0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1}),
+	NewWheel([]int{0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0}),
+	NewWheel([]int{1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1}),
+	NewWheel([]int{1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0}),
+	NewWheel([]int{1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1}),
+}
+
+func Test_LoadingCiphertext(t *testing.T) {
+
+    ciphertext, plaintext := parseCiphertext(TEST_CIPHERTEXT_FILE)
+    log.Print(ciphertext[:20])
+
+    //Plaintext is generated from reading ciphertext
+    //We know it starts off with UMUM4VEVE35
+    if plaintext[:11] != "UMUM4VEVE35" {
+        t.Error("Error reading test ciphertext")
+    }
+    if ciphertext[:11] != "BTEVUIO7WGR"{
+        t.Error("Error reading test ciphertext")
+    }
+}
+
+
+func Test_WheelsEqual(t *testing.T){
+    wheel := NewWheel([]int{0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0})
+    wheel2 := NewWheel([]int{0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0})
+    wheel3 := NewWheel([]int{1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0})
+    if !wheel.Equals(*wheel) {
+        t.Error("Wheel equality failed - wheel is not equal to itself")
+    }
+
+    if !wheel.Equals(*wheel2) {
+        t.Error("Wheel equality failed - wheel is not equal to identical other wheel")
+    }
+
+    if wheel.Equals(*wheel3) || wheel2.Equals(*wheel3){
+        t.Error("Wheel equality failed - wheel is equal to a non-identical other wheel")
+    }
+
+
+}
+
+func Test_Decryption(t *testing.T) {
+
+    wheels := crackMessage(TEST_CIPHERTEXT_FILE)
+
+    //Plaintext is generated from reading ciphertext
+    //We know it starts off with UMUM4VEVE35
+    for i, wheel := range wheels{
+        if !wheel.Equals(*TEST_CIPHERTEXT_SOLVED_WHEELS[i]){
+            t.Error("Error decoding ciphertext: wheel %d does not match expected result")
+        }
+    }
+}
